f4a52880ffdb7cf04fd0241af84ceaf0b94e0ffd
BillyAndrys <billyandrys@gmail.com>
2015-02-12 21:05:36 -0500

    refactorizacion de page git

---
+++ js/js.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    var lg = $('.container');
+    lg.hide();
+    lg.fadeIn(90000, function(){
+        lg.show(200);
+    });
+    
+});
+
+
+function Datos(){
+  
+  $("video").fadeOut("slow");
+  
+
+}
